4278df4eb5e43d204a3d3e287e29edb6b397462b
Zakaria Khourbach <zkhourba@e1r6p1.1337.ma>
2025-02-20 18:47:28 +0100

    fix dely

--- mandatory/write_status.c
+++ mandatory/write_status.c
@@ -6,7 +6,7 @@
 /*   By: zkhourba <zkhourba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:02:55 by zkhourba          #+#    #+#             */
-/*   Updated: 2025/02/19 18:19:42 by zkhourba         ###   ########.fr       */
+/*   Updated: 2025/02/20 18:47:15 by zkhourba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 void	write_status(t_philo *p, int status)
 {
-	size_t	elapsed;
+	size_t		elapsed;
+	static int	f;
 
 	pthread_mutex_lock(&p->data->philo_mtx);
 	elapsed = get_current_time() - p->data->start_time;
+	if (f == 0)
+		elapsed = 0;
+	f++;
 	if (status == EAT && is_not_finsh(&p->data->finsh_mtx, &p->data->isfinsh))
 		printf("%ld %d is eating\n", elapsed, p->philo_id);
 	if (status == DIE && is_not_finsh(&p->data->finsh_mtx, &p->data->isfinsh))
