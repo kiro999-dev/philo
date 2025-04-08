56b35845eb46fdc97b940404935a7ff675b00d81
Zakaria Khourbach <zkhourba@e1r5p5.1337.ma>
2025-04-08 12:45:38 +0100

    fix problem of delay :)

--- philo/mandatory/write_status.c
+++ philo/mandatory/write_status.c
@@ -6,7 +6,7 @@
 /*   By: zkhourba <zkhourba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:02:55 by zkhourba          #+#    #+#             */
-/*   Updated: 2025/02/20 18:47:15 by zkhourba         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:43:38 by zkhourba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@
 void	write_status(t_philo *p, int status)
 {
 	size_t		elapsed;
-	static int	f;
 
 	pthread_mutex_lock(&p->data->philo_mtx);
 	elapsed = get_current_time() - p->data->start_time;
-	if (f == 0)
-		elapsed = 0;
-	f++;
 	if (status == EAT && is_not_finsh(&p->data->finsh_mtx, &p->data->isfinsh))
 		printf("%ld %d is eating\n", elapsed, p->philo_id);
 	if (status == DIE && is_not_finsh(&p->data->finsh_mtx, &p->data->isfinsh))
--- philo_bonus/bonus/philo_bonus.c
+++ philo_bonus/bonus/philo_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zkhourba <zkhourba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:08:51 by zkhourba          #+#    #+#             */
-/*   Updated: 2025/04/07 13:23:13 by zkhourba         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:44:53 by zkhourba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	take_forks(t_philo_bonus *p)
 {
+	
 	sem_wait(p->data->forks);
 	write_status(p, TAKE_FORK);
 	sem_wait(p->data->forks);
@@ -26,8 +27,8 @@ void	eating(t_philo_bonus *p)
 	write_status(p, EAT);
 	p->meal_count++;
 	p->last_eat = get_current_time();
-	sem_post(p->data->check);
 	ft_usleep(p->data->time_to_eat);
+	sem_post(p->data->check);
 	sem_post(p->data->forks);
 	sem_post(p->data->forks);
 }
@@ -71,11 +72,12 @@ void	start_syc(t_data *d)
 int	main(int argc, char **argv)
 {
 	t_data	data;
-
+	
 	if (parsing(argc, argv, &data))
 		return (1);
 	if (init_data(&data))
 		return (1);
+	
 	sem_wait(data.finish);
 	data.start_time = get_current_time();
 	start_syc(&data);
